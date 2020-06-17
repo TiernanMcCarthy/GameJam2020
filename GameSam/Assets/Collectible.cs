84142d9f55b72c76e639938e8d860630d17a9287
Tiernan <tiernanmccarthy@outlook.com>
2020-06-17 13:18:06 +0100

    Collectibles

---
+++ GameSam/Assets/Collectible.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public CollectibleChecker Checker;
+
+
+    void Start()
+    {
+        Checker.Collectables.Add(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.GetComponent<characterMovement>())
+        {
+
+        }
+    }
+}
